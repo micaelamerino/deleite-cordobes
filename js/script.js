e1706737ed655733941d00e7abe34b1bffe7cdad
mica <ma.mica.merino@gmail.com>
2023-03-24 17:20:38 -0300

    Correción linkeo

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const cantidadCarrito = document.getElementById("cantidadCarrito");
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
 const getProducts = async () => {
-    const response = await fetch("../data.json");
+    const response = await fetch("./data.json");
     const data = await response.json();
     data.forEach((product) => {
         let content = document.createElement("div");
