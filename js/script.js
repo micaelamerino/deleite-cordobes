5e13b90ccf3a1b4ab5a0c74de71398a11a9abfff
mica <ma.mica.merino@gmail.com>
2023-04-03 22:21:18 -0300

    Se agregó alerta en suscripción

--- js/script.js
+++ js/script.js
@@ -6,57 +6,75 @@ const cantidadCarrito = document.getElementById("cantidadCarrito");
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
 const getProducts = async () => {
-    const response = await fetch("./data.json");
-    const data = await response.json();
-    data.forEach((product) => {
-        let content = document.createElement("div");
-        content.className = "card text-center";
-        content.innerHTML = `
+  const response = await fetch("./data.json");
+  const data = await response.json();
+  data.forEach((product) => {
+    let content = document.createElement("div");
+    content.className = "card text-center";
+    content.innerHTML = `
         <img src= "${product.img}">
         <h5 class="subtitulo mt-2">${product.nombre}</h5>
         <p class="parrafo">${product.unidades}</p>
         <p class="parrafo">$ ${product.precio}</p>
         `;
-        contentCards.append(content);
+    contentCards.append(content);
 
-        let btnAgregar = document.createElement("button");
-        btnAgregar.className = "btn-agregar";
-        btnAgregar.innerText = "Agregar al carrito";
-        content.appendChild(btnAgregar);
+    let btnAgregar = document.createElement("button");
+    btnAgregar.className = "btn-agregar";
+    btnAgregar.innerText = "Agregar al carrito";
+    content.appendChild(btnAgregar);
 
-        contentCards.append(content);
+    contentCards.append(content);
 
-        btnAgregar.addEventListener("click", () => {
-
-            const repeat = carrito.some((repeatProduct) => repeatProduct.id === product.id);
-            if (repeat) {
-                carrito.map((prod) => {
-                    if (prod.id === product.id) {
-                        prod.cantidad++
-                    };
-                });
-            } else {
-                carrito.push({
-                    id: product.id,
-                    img: product.img,
-                    nombre: product.nombre,
-                    unidades: product.unidades,
-                    precio: product.precio,
-                    cantidad: product.cantidad
-                });
-                carritoContador();
-                saveLocal();
-            };
+    btnAgregar.addEventListener("click", () => {
+      const repeat = carrito.some(
+        (repeatProduct) => repeatProduct.id === product.id
+      );
+      if (repeat) {
+        carrito.map((prod) => {
+          if (prod.id === product.id) {
+            prod.cantidad++;
+          }
         });
+      } else {
+        carrito.push({
+          id: product.id,
+          img: product.img,
+          nombre: product.nombre,
+          unidades: product.unidades,
+          precio: product.precio,
+          cantidad: product.cantidad,
+        });
+        carritoContador();
+        saveLocal();
+      }
     });
+  });
 };
 
 getProducts();
 
 const saveLocal = () => {
-    localStorage.setItem("carrito", JSON.stringify(carrito));
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+};
+
+const btnSuscripcion = document.getElementById("btnSuscripcion");
+
+function confirmSusc(e) {
+  e.preventDefault();
+  const email = document.getElementById("email").value;
+  if (email == "") {
+    Swal.fire({
+        icon: 'error',
+        title: 'ERROR',
+        text: 'Campo vacío, vuelva a intentar',
+      });
+  } else {
+    Swal.fire({
+        icon: 'success',
+        title: '¡Bienvenido!',
+        text: 'Suscripción realizada exitosamente',
+      });
+  }
 }
-
-
-
-
+btnSuscripcion.addEventListener("click", confirmSusc);
